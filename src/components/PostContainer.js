1a926615a794e838ed60243f8d0f924ee373b874
Faisal Karim <faisalchy64@gmail.com>
2023-02-18 14:12:09 +0600

    i have fetched all posts on the postcontainer component

--- src/components/PostContainer.js
+++ src/components/PostContainer.js
@@ -1,11 +1,21 @@
+import { useEffect, useState } from "react";
+import { url } from "../common";
 import Post from "./Post";
 
 function PostContainer() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        fetch(`${url}/posts`)
+            .then((res) => res.json())
+            .then((data) => setPosts(data));
+    }, []);
+
     return (
         <section className="w-4/5 sm:w-[500px] flex flex-col gap-5 mx-auto py-5">
-            <Post />
-            <Post />
-            <Post />
+            {posts.map((post) => (
+                <Post key={post._id} post={post} />
+            ))}
         </section>
     );
 }
